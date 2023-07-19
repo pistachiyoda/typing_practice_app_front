f07c1b67f4153df1c6809fd92c8f59d5826da6b2
mai <fujisawamai21@gmail.com>
2023-07-19 23:08:33 +0900

    loginの実装

--- src/component/Auth.tsx
+++ src/component/Auth.tsx
@@ -1,6 +1,40 @@
 import { Button, Stack, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+import axios from "axios";
 
 export const Auth: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmailChange = (e: any) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e: any) => {
+    setPassword(e.target.value);
+  };
+
+  const handleLogin = () => {
+    const formData = new URLSearchParams();
+    formData.append("email", email);
+    formData.append("password", password);
+    try {
+      const jwt_token = axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
+        formData,
+        {
+          headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+          },
+          withCredentials: true,
+        }
+      );
+      console.log("login!");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <>
       <Stack direction="row" spacing={2}>
@@ -8,9 +42,19 @@ export const Auth: React.FC = () => {
           Sign up
         </Button>
         <span>OR</span>
-        <TextField id="foutlined-basic" label="email" variant="outlined" />
-        <TextField id="foutlined-basic" label="password" variant="outlined" />
-        <Button variant="contained" href="./signup">
+        <TextField
+          id="foutlined-basic"
+          label="email"
+          variant="outlined"
+          onChange={handleEmailChange}
+        />
+        <TextField
+          id="foutlined-basic"
+          label="password"
+          variant="outlined"
+          onChange={handlePasswordChange}
+        />
+        <Button variant="contained" onClick={handleLogin}>
           Login
         </Button>
       </Stack>
